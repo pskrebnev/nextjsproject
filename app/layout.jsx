d4c79cb73396f87b1a6a622968631444bfaaaf42
PSkrabneu <skrebnev@gmail.com>
2024-10-31 11:28:03 +0100

    Init commit

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import '@/assets/styles/global.css';
+
+export const metadata = {
+    title: 'Property Pulse',
+    description: 'Find perfect property for your needs',
+    keywords: 'property, pulse, real estate, rental, sale, buy, rent, sell',
+};
+
+const MainLayout = ({ children }) => {
+    return (
+        <html>
+            <body>
+                <main>{children}</main>
+            </body>
+        </html>
+    );
+};
+
+export default MainLayout;
+
+
+
