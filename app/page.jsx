78e8f6d5ca010001526fe79eb220ad3c2719f71b
PSkrabneu <skrebnev@gmail.com>
2024-10-31 12:39:13 +0100

    Properties added

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,20 @@
+import Link from 'next/link';
+
 const HomePage = () => {
-    return <div className="text-2xl">Home Page</div>;
+    return (
+        <div>
+            <h1 className="text-3xl">Wellcome to Property Pulse</h1>
+            <Link href="/properties">Go to Properties</Link>
+            {/* <Link
+                href={{
+                    pathname: 'properties',
+                    query: {
+                        name: 'TEST'
+                    },
+                }}
+            >Add Property</Link> */}
+        </div>
+    );
 };
 
 export default HomePage;
---
+++ app/properties/page.jsx
@@ -0,0 +1,8 @@
+const PropertiesPage = () => {
+    return <div>Properties Page</div>;
+};
+
+export default PropertiesPage;
+
+
+
