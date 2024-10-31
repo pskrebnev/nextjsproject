b04b244553ac6db5fc2eaa9a62b985b968e809eb
PSkrabneu <skrebnev@gmail.com>
2024-10-31 16:00:03 +0100

    Navigation changed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,21 @@
-import Navbar from '@/components/navbar';
+import Navbar from '@/components/Navbar';
 import '@/assets/styles/global.css';
 
 export const metadata = {
-    title: 'Property Pulse',
-    description: 'Find perfect property for your needs',
-    keywords: 'property, pulse, real estate, rental, sale, buy, rent, sell',
+  title: 'Property Pulse',
+  description: 'Find perfect property for your needs',
+  keywords: 'property, pulse, real estate, rental, sale, buy, rent, sell',
 };
 
 const MainLayout = ({ children }) => {
-    return (
-        <html>
-            <body>
-                <Navbar />
-                <main>{children}</main>
-            </body>
-        </html>
-    );
+  return (
+    <html>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
 };
 
 export default MainLayout;
-
-
-
