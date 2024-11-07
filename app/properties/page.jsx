eeb005c1a903402e6625a27c58501cde98f1b885
PSkrabneu <skrebnev@gmail.com>
2024-11-07 12:50:05 +0100

    'Recent Properties' added

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,8 +1,22 @@
+import PropertyCard from '@/components/PropertyCard';
+import properties from '@/app/properties/properties.json';
+
 const PropertiesPage = () => {
-    return <div>Properties Page</div>;
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <div>No properties found</div>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard property={property} key={property._id} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default PropertiesPage;
-
-
-
