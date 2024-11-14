813cc4ac044ac4e52b8760cdf424e414e9b0b63a
PSkrabneu <skrebnev@gmail.com>
2024-11-14 17:33:26 +0100

    Icons implemented

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,4 +1,5 @@
 import PropertyHeaderImage from '@/components/PropertyHeaderImage';
+import PropertyDetails from '@/components/PropertyDetails';
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
 import Link from 'next/link';
@@ -27,7 +28,7 @@ const PropertyPage = async ({ params }) => {
       <section className="bg-blue-50">
         <div className="container m-auto py-10 px-6">
           <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-            {/* Property Info here */}
+            <PropertyDetails property={property} />
           </div>
         </div>
       </section>
