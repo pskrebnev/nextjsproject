f828c959cf158e81f5b1c846edc22ffdd324bcda
PSkrabneu <skrebnev@gmail.com>
2024-11-14 15:55:11 +0100

    Added background image

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,26 +1,38 @@
-'use client';
-import { useRouter, useParams, useSearchParams, usePathname } from 'next/navigation';
+import PropertyHeaderImage from '@/components/PropertyHeaderImage';
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
+import Link from 'next/link';
+import { FaArrowLeft } from 'react-icons/fa';
 
-const PropertyPage = () => {
-    const router = useRouter();
-    console.log(router);
-    const params = useParams();
-    console.log(params);
-    const searchParams = useSearchParams();
-    console.log(searchParams);
-    const pathname = usePathname();
-    console.log(pathname);
-    return (
-        <div>
-            
-            <button onClick={() => router.replace('/')}>Go to Home</button><br>
-            </br>
-            Property Page {params.id}, {searchParams.get('name')};
-            <br />
-            Pathname: {pathname};
+const PropertyPage = async ({ params }) => {
+  await connectDB();
+  const { id } = await params;
+  const property = await Property.findById(id).lean();
+
+  return (
+    <>
+      <PropertyHeaderImage image={property.images[0]} />
+
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            href="/properties"
+            className="text-blue-500 hover:text-blue-600 flex items-center"
+          >
+            <FaArrowLeft className="mr-2" /> Back to Properties
+          </Link>
         </div>
-    );
+      </section>
+
+      <section className="bg-blue-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            {/* Property Info here */}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default PropertyPage;
-
